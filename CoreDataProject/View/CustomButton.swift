c14dc5bd2b57309ceedad61488d45e0c360a6793
Lucas Moraes <lucasvs.moraes@hotmail.com>
2017-12-22 20:26:00 -0200

    Fixed some syntax and deleting the uibutton extension

--- CoreDataProject/View/CustomButton.swift
+++ CoreDataProject/View/CustomButton.swift
@@ -8,14 +8,14 @@
 
 import UIKit
 
-var isRouded = false
+var isRounded = false
 
 @IBDesignable class CustomButton: UIButton {
     @IBInspectable var cornerR: Bool {
-        get { return isRouded }
+        get { return isRounded }
         set {
-            isRouded = newValue
-            if isRouded == true {
+            isRounded = newValue
+            if isRounded == true {
                 self.layer.cornerRadius = self.frame.height/2
             } else {
                 self.layer.cornerRadius = 0
@@ -23,23 +23,3 @@ var isRouded = false
         }
     }
 }
-
-extension UIButton {
-    func bindToKeyboard() {
-        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillChange(_:)), name: NSNotification.Name.UIKeyboardWillChangeFrame, object: nil)
-    }
-    
-    @objc func keyboardWillChange(_ notification: NSNotification) {
-        
-        let duration = notification.userInfo![UIKeyboardAnimationDurationUserInfoKey] as! Double
-        let curve = notification.userInfo![UIKeyboardAnimationCurveUserInfoKey] as! UInt
-        let beginningFrame = (notification.userInfo![UIKeyboardFrameBeginUserInfoKey] as! NSValue).cgRectValue
-        let endFrame = (notification.userInfo![UIKeyboardFrameEndUserInfoKey] as! NSValue).cgRectValue 
-        let deltaY = endFrame.origin.y - beginningFrame.origin.y
-        
-        UIView.animateKeyframes(withDuration: duration, delay: 0.0, options: UIViewKeyframeAnimationOptions(rawValue: curve), animations: {
-            self.frame.origin.y += deltaY
-        }, completion: nil)
-    }
-}
-
